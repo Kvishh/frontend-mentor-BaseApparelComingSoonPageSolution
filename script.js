c4b8cfe24a9c0e5b3a96b392c348d8cdd31ef556
hsiV <kelvishvargas23@gmail.com>
2025-10-02 19:04:06 +0800

    Fixed the padding of textfield form when the error icon pops up. Also added 'enter' keyevent for textfield form

--- script.js
+++ script.js
@@ -4,17 +4,23 @@ const textField = document.querySelector("#email-form");
 const errorAlert = document.querySelector(".error-alert-toggle");
 const errorIcon = document.querySelector(".error-icon-wrapper-toggle");
 
-submitBtn.addEventListener("click", () => {
+submitBtn.addEventListener("click", validateEmail);
+textField.addEventListener("keydown", (e) => {
+    if (e.key == "Enter"){
+        validateEmail();
+    }
+});
 
-    if (textField.value.includes("@")){
+function validateEmail() {
+    if (textField.value.includes("@")) {
         textField.value = "";
         errorAlert.style.display = "none";
         errorIcon.style.display = "none";
         textField.style.borderColor = "hsl(0, 0%, 82%)"
     }
-    else if (!textField.value.includes("@") || !(textField.value.trim().length === 0)){
+    else if (!textField.value.includes("@") || !(textField.value.trim().length === 0)) {
         errorAlert.style.display = "block";
         errorIcon.style.display = "block";
         textField.style.borderColor = "hsl(0, 93%, 68%)"
     }
-});
+}
